00cbd720899078244af522b2e16c4848c9b5892c
haeuniea <haeunhaa@gmail.com>
2026-08-12 14:48:17 +0900

    HelloController에 Exception 예외 처리 코드 추가

--- src/main/java/com/example/springpractice/controller/HelloController.java
+++ src/main/java/com/example/springpractice/controller/HelloController.java
@@ -2,10 +2,17 @@ package com.example.springpractice.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloController {
@@ -24,5 +31,28 @@ public class HelloController {
         LOGGER.info("Info log");
         LOGGER.warn("Warn log");
         LOGGER.error("Error log");
+    }
+
+    @PostMapping("/exception")
+    public void exceptionTest() throws Exception {
+        throw new Exception();
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<Map<String, String>> exceptionHandler(Exception e) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        //responseHeaders.add(HttpHeaders.CONTENT_TYPE, "application/json");
+
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        LOGGER.info(e.getMessage());
+        LOGGER.info("Controller 내 ExceptionHandler 호출");
+
+        Map<String, String> map = new HashMap<>();
+        map.put("error type", httpStatus.getReasonPhrase());
+        map.put("code", "400");
+        map.put("message", "에러 발생");
+
+        return new ResponseEntity<>(map, responseHeaders, httpStatus);
     }
 }
